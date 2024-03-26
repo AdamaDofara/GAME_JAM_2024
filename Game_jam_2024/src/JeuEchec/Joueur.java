669c7ee1dbf4207906c5d60e559d4afd576c5cef
Slynhive <165002420+Slynhive@users.noreply.github.com>
2024-03-26 12:37:49 +0100

    j ai mis les class principales qui sont utilisées , a voir si il faut en ajouter d'autre et bien sur il faut modif tout ça

---
+++ Game_jam_2024/src/JeuEchec/Joueur.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package JeuEchec;
+
+/**
+ *
+ * @author susuf
+ */
+
+  
+
+/**
+ * Defini un joueur humain
+ */
+public class Joueur {
+
+	/**
+	 * Defini si le Joueur est humain ou non
+	 */
+
+	protected boolean estHumain = true;
+	
+	/**
+	 * Couleur du joueur
+	 */
+	protected String couleur;
+	
+	/**
+	 * Constructeur
+	 */
+	public Joueur(){
+		
+	}
+	
+	/**
+	 * Instancie un joueur selon la couleur c
+	 * @param c couleur du joueur
+	 */
+	public Joueur(String c){
+		this.couleur = c;
+	}
+
+	/**
+	 * Accesseur de la couleur du joueur
+	 * @return couleur du joueur
+	 */
+	public String getCouleur(){
+		return couleur;
+	}
+
+	/**
+	 * Retourne si le joueur est humain
+	 * @return True si il l'est sinon false
+	 */
+	public boolean estHumain(){
+		return estHumain;
+	}
+
+	/** 
+	 * Defini si le Joueur est humain
+	 * @param b
+	 */
+	public void setHumain(boolean b){
+		estHumain = b;
+	}
+}
+
