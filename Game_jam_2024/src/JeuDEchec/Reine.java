62d76b9ebe62a8fe11ef9f09541640254a7c625e
AdamaDofara <86635119+AdamaDofara@users.noreply.github.com>
2024-03-27 11:47:28 +0100

    mise à jour des classes Reine, cavalier

--- Game_jam_2024/src/JeuDEchec/Reine.java
+++ Game_jam_2024/src/JeuDEchec/Reine.java
@@ -4,14 +4,72 @@
  */
 package JeuDEchec;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author susuf
  */
 
-
- 
 public class Reine extends Piece {
+	public ArrayList<Coordonnee> deplacementPossible(){
+		int x = this.x+1;
+    	int y = this.y;
+    	ArrayList<Coordonnee> possible = new ArrayList<Coordonnee>();
+    	while(x<8 && x>=0 && y>=0&& y<8 && verifier(x, y)=="rien" || verifier( x,  y)=="Enemi" && (plateau.getpieceAt(x, y) == null || plateau.getpieceAt(x, y).getcouleur() != this.getcouleur())){
+    		possible.add(new Coordonnee(x,y));
+    		x++;
+    	}
+    	x = this.x-1;
+    	
+    	while(x<8 && x>=0 && y>=0&& y<8 && verifier(x, y)=="rien" || verifier( x,  y)=="Enemi" && (plateau.getpieceAt(x, y) == null || plateau.getpieceAt(x, y).getcouleur() != this.getcouleur())){
+    		possible.add(new Coordonnee(x,y));
+    		x--;
+    	}
+    	x = this.x;
+    	y = this.y+1;
+    	while(x<8 && x>=0 && y>=0&& y<8 && verifier(x, y)=="rien" || verifier( x,  y)=="Enemi" && (plateau.getpieceAt(x, y) == null || plateau.getpieceAt(x, y).getcouleur() != this.getcouleur())){
+    		possible.add(new Coordonnee(x,y));
+    		y++;
+    	}
+    	x = this.x;
+    	y = this.y-1;
+    	while(x<8 && x>=0 && y>=0&& y<8 && verifier(x, y)=="rien" || verifier( x,  y)=="Enemi" && (plateau.getpieceAt(x, y) == null || plateau.getpieceAt(x, y).getcouleur() != this.getcouleur())){
+    		possible.add(new Coordonnee(x,y));
+    		y--;
+    	}
+    	x = this.x+1;
+    	y = this.y+1;
+    	while(x<8 && x>=0 && y>=0&& y<8 && verifier(x, y)=="rien" || verifier( x,  y)=="Enemi" && (plateau.getpieceAt(x, y) == null || plateau.getpieceAt(x, y).getcouleur() != this.getcouleur())){
+    		possible.add(new Coordonnee(x,y));
+    		x++;
+    		y++;
+    	}
+    	x = this.x-1;
+    	y = this.y-1;
+    	while(x<8 && x>=0 && y>=0&& y<8 && verifier(x, y)=="rien" || verifier( x,  y)=="Enemi" && (plateau.getpieceAt(x, y) == null || plateau.getpieceAt(x, y).getcouleur() != this.getcouleur())){
+    		possible.add(new Coordonnee(x,y));
+    		x--;
+    		y--;
+    	}
+    	
+    	x = this.x-1;
+    	y = this.y+1;
+    	while(x<8 && x>=0 && y>=0&& y<8 && verifier(x, y)=="rien" || verifier( x,  y)=="Enemi" && (plateau.getpieceAt(x, y) == null || plateau.getpieceAt(x, y).getcouleur() != this.getcouleur())){
+    		possible.add(new Coordonnee(x,y));
+    		x--;
+    		y++;
+    	}
+    	x = this.x+1;
+    	y = this.y-1;
+    	while(x<8 && x>=0 && y>=0&& y<8 && verifier(x, y)=="rien" || verifier( x,  y)=="Enemi" && (plateau.getpieceAt(x, y) == null || plateau.getpieceAt(x, y).getcouleur() != this.getcouleur())){
+    		possible.add(new Coordonnee(x,y));
+    		x++;
+    		y--;
+    	}
+    	return possible;
+    }
+		
     public Reine(int pDv, int pDa, int pDd, String c, Plateau p, int x, int y) {
        
         super(pDv, pDa, pDd, c, p, x, y);
