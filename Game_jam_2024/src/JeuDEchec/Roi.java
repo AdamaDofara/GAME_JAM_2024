9a84e25b03257ba1b0c2689517fb5df1d2d7b1b8
AdamaDofara <86635119+AdamaDofara@users.noreply.github.com>
2024-03-27 12:25:33 +0100

    Mise à jour des classes Roi et Tour

--- Game_jam_2024/src/JeuDEchec/Roi.java
+++ Game_jam_2024/src/JeuDEchec/Roi.java
@@ -4,9 +4,11 @@
  */
 package JeuDEchec;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author susuf
+ * @author Kenny && Adama
  */
 
  
@@ -16,6 +18,38 @@ package JeuDEchec;
  * Defini le comportement du roi
  */
 public class Roi extends Piece{
+	
+	
+	 public ArrayList<Coordonnee> deplacementPossible(){//
+		 ArrayList<Coordonnee> possible = new ArrayList<Coordonnee>();
+     	if(((x) >= 0 && (y+1) >= 0 && (x) < 8 && (y+1) < 8) && (this.plateau.getpieceAt(x,y+1) == null || !this.plateau.getpieceAt(x,y+1).couleur.equals(this.couleur))){
+     		possible.add(new Coordonnee(x,y+1));
+     	}
+     	if(((x) >= 0 && (y-1) >= 0 && (x) < 8 && (y-1) < 8) && (this.plateau.getpieceAt(x,y-1) == null || !this.plateau.getpieceAt(x,y-1).couleur.equals(this.couleur))){
+     		possible.add(new Coordonnee(x,y-1));
+     	}
+     	if(((x+1) >= 0 && (y) >= 0 && (x+1) < 8 && (y) < 8) && (this.plateau.getpieceAt(x+1,y) == null || !this.plateau.getpieceAt(x+1,y).couleur.equals(this.couleur))){
+     		possible.add(new Coordonnee(x+1,y));
+     	}
+     	if(((x-1) >= 0 && (y) >= 0 && (x-1) < 8 && (y) < 8) && (this.plateau.getpieceAt(x-1,y) == null || !this.plateau.getpieceAt(x-1,y).couleur.equals(this.couleur))){
+     		possible.add(new Coordonnee(x-1,y));
+     	}
+     	if(((x+1) >= 0 && (y+1) >= 0 && (x+1) < 8 && (y+1) < 8) && (this.plateau.getpieceAt(x+1,y+1) == null || !this.plateau.getpieceAt(x+1,y+1).couleur.equals(this.couleur))){
+     		possible.add(new Coordonnee(x+1,y+1));
+     	}
+     	if(((x-1) >= 0 && (y+1) >= 0 && (x-1) < 8 && (y+1) < 8) && (this.plateau.getpieceAt(x-1,y+1) == null || !this.plateau.getpieceAt(x-1,y+1).couleur.equals(this.couleur))){
+     		possible.add(new Coordonnee(x-1,y+1));
+     	}
+     	if(((x+1) >= 0 && (y-1) >= 0 && (x+1) < 8 && (y-1) < 8) && (this.plateau.getpieceAt(x+1,y-1) == null || !this.plateau.getpieceAt(x+1,y-1).couleur.equals(this.couleur))){
+     		possible.add(new Coordonnee(x+1,y-1));
+     	}
+     	if(((x-1) >= 0 && (y-1) >= 0 && (x-1) < 8 && (y-1) < 8) && (this.plateau.getpieceAt(x-1,y-1) == null || !this.plateau.getpieceAt(x-1,y-1).couleur.equals(this.couleur))){
+     		possible.add(new Coordonnee(x-1,y-1));
+     	}
+     	
+     	return possible;
+	    
+	    }
 	public Roi(int pDv, int pDa, int pDd, String c, Plateau p, int x, int y) {
        
         super(pDv, pDa, pDd, c, p, x, y);
--- Game_jam_2024/src/JeuDEchec/Tour.java
+++ Game_jam_2024/src/JeuDEchec/Tour.java
@@ -4,9 +4,11 @@
  */
 package JeuDEchec;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author susuf
+ * @author Adama && Kenny
  */
 
   
@@ -18,6 +20,36 @@ package JeuDEchec;
  * Defini le comportement de la Tour
  */
 public class Tour extends Piece{
+	
+	public ArrayList<Coordonnee>deplacementPossible (){
+    	int x = this.x+1;
+    	int y = this.y;
+    	ArrayList<Coordonnee> possible = new ArrayList<Coordonnee>();
+    	while(x<8 && x>=0 && y>=0&& y<8 && verifier(x, y)=="rien" || verifier( x,  y)=="Enemi" && (plateau.getpieceAt(x, y) == null || plateau.getpieceAt(x, y).getcouleur() != this.getcouleur())){
+    		possible.add(new Coordonnee(x,y));
+    		x++;
+    	}
+    	x = this.x-1;
+    	
+    	while(x<8 && x>=0 && y>=0&& y<8 && verifier(x, y)=="rien" || verifier( x,  y)=="Enemi" && (plateau.getpieceAt(x, y) == null || plateau.getpieceAt(x, y).getcouleur() != this.getcouleur())){
+    		possible.add(new Coordonnee(x,y));
+    		x--;
+    	}
+    	x = this.x;
+    	y = this.y+1;
+    	while(x<8 && x>=0 && y>=0&& y<8 && verifier(x, y)=="rien" || verifier( x,  y)=="Enemi" && (plateau.getpieceAt(x, y) == null || plateau.getpieceAt(x, y).getcouleur() != this.getcouleur())){
+    		possible.add(new Coordonnee(x,y));
+    		y++;
+    	}
+    	x = this.x;
+    	y = this.y-1;
+    	while(x<8 && x>=0 && y>=0&& y<8 && verifier(x, y)=="rien" || verifier( x,  y)=="Enemi" && (plateau.getpieceAt(x, y) == null || plateau.getpieceAt(x, y).getcouleur() != this.getcouleur())){
+    		possible.add(new Coordonnee(x,y));
+    		y--;
+    	}
+    	return possible;
+    }
+	
 	public Tour(int pDv, int pDa, int pDd, String c, Plateau p, int x, int y) {
        
         super(pDv, pDa, pDd, c, p, x, y);
